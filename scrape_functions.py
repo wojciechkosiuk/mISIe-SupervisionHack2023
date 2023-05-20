d3686a26f03f11aa631ebf78a6f2d3048ee1eec0
Adam Majczyk <adam.majczyk2001@gmail.com>
2023-05-20 21:47:59 +0200

    add html save func

--- scrape_functions.py
+++ scrape_functions.py
@@ -16,3 +16,11 @@ def return_soup(url):
     # Create a BeautifulSoup object
     soup = BeautifulSoup(html_content, 'html.parser')
     return soup
+
+def save_url_to_html(url, filename):
+    # current date and time
+    
+    # save to html
+    soup = return_soup(url)
+    with open(f'htmls/{datetime.datetime.now().date()}-{filename}.html', 'w', encoding='utf-8') as f:
+        f.write(str(soup))
